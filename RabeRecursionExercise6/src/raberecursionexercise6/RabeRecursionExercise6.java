86bc465b51498cb62ca8836576ee3bef9508fdfe
AgentOJR <owen.rabe@icloud.com>
2023-04-20 10:16:40 -0400

    exersise 6 done

---
+++ RabeRecursionExercise6/src/raberecursionexercise6/RabeRecursionExercise6.java
@@ -0,0 +1,41 @@
+/*
+ * Owen Rabe
+ * Apr 20 23
+ * Square fractal drawing problem
+ */
+package raberecursionexercise6;
+
+import TurtleGraphics.*;
+
+
+public class RabeRecursionExercise6 {
+
+    
+    public static void main(String[] args) {
+        StandardPen p = new StandardPen(500,500);
+        fractal(0,0,100,p);
+        
+    }
+    public static void drawSquare(StandardPen p,int x,int y,int r){
+        p.up();
+        p.move(x-r,y+r);
+        p.down();
+        p.move(x+r,y+r);
+        p.move(x+r,y-r);
+        p.move(x-r,y-r);
+        p.move(x-r,y+r);
+    }
+    public static void fractal(int x,int y,int r,StandardPen p){
+        if(r==0){
+            
+        }else{
+            drawSquare(p,x,y,r);
+            fractal(x-r,y+r,r/2,p);
+            fractal(x-r,y-r,r/2,p);
+            fractal(x+r,y+r,r/2,p);
+            fractal(x+r,y-r,r/2,p);
+        }
+    }
+        
+    
+}
