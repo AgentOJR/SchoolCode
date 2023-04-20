4e3d85b09bc9f2c693db94bf8b506e8bb9f37f2a
AgentOJR <owen.rabe@icloud.com>
2023-04-20 14:46:25 -0400

    new stuff too

---
+++ RabeRecursionChallenge/src/raberecursionchallenge/RabeRecursionChallenge.java
@@ -0,0 +1,23 @@
+/*
+ * \Owen Rabe
+ * Apr 20 23
+ * Sierpinski's Triangle Drawer
+ */
+package raberecursionchallenge;
+
+
+public class RabeRecursionChallenge {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static void drawSierpinski(int x1,int y1,int x2,int y2,int x3,int y3,int level){
+        if(level > 1){
+            
+        }else{
+            
+        }
+    }
+    
+}
