412598aab6caa43c9418a8a1935d0686f02623b6
AgentOJR <owen.rabe@icloud.com>
2023-05-01 10:10:34 -0400

    sorting exercise 3 done

---
+++ RabeSortingExercise3/src/rabesortingexercise3/RabeSortingExercise3.java
@@ -0,0 +1,48 @@
+/**
+ * Owen Rabe
+ * May 1 23
+ * Numbers with insertion sort
+ */
+package rabesortingexercise3;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class RabeSortingExercise3 {
+
+    static int[] a = new int[1000];
+    public static void main(String[] args) {
+        try {
+            File f = new File("src/rabesortingexercise3/randomNumbers.txt");
+            Scanner s = new Scanner(f);
+            loadArray(s);
+            insertSort(a.length);
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(a[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+    public static void loadArray(Scanner s){
+        for (int i = 0; i < 1000; i++) {
+            a[i] = Integer.parseInt(s.nextLine());
+        }
+    }
+    public static void insertSort(int numItems){
+        int temp;
+        int j;
+        for (int i = 1; i < numItems; i++) {
+            temp = a[i];
+            j = i -1;
+            while(j >= 0 && a[j] > temp){
+                a[j+1] = a[j];
+                j = j-1;
+            }
+            a[j+1] = temp;
+        }
+    }
+        
+    
+}
